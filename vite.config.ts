c9c88475bb9768e133c6f85c0c23bfec68c56fa5
ReshNikita <rieshietnikov.nikita.1998@mail.ru>
2023-09-30 09:11:47 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Youtube-clone/",
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Youtube-clone/",
 
-  resolve: {
-    alias: {
-      web3: "web3/dist/web3.min.js",
-    },
-  },
+//   resolve: {
+//     alias: {
+//       web3: "web3/dist/web3.min.js",
+//     },
+//   },
+// });
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/Youtube-clone/";
+  }
+
+  return config;
 });
